deba6d1b1b5419621181ab9ec786895c8f6fa5a8
akashin <kashin.andrej@gmail.com>
2020-08-30 15:56:37 +0100

    [blinky] Implement phase3

--- phase3/blinky.c
+++ phase3/blinky.c
@@ -15,6 +15,15 @@ static void spin_sleep_ms(unsigned int ms) {
 }
 
 int main(void) {
-  // FIXME: STEP 1: Set GPIO Pin 16 as output.
-  // FIXME: STEP 2: Continuously set and clear GPIO 16.
+  // Set GPIO Pin 16 as output.
+  *GPIO_FSEL1 |= 0b001 << 18;
+  *GPIO_FSEL1 &= 0b001 << 18;
+
+  // Continuously set and clear GPIO 16.
+  while (1) {
+    *GPIO_SET0 |= 1<<16;
+    spin_sleep_ms(1000);
+    *GPIO_CLR0 |= 1<<16;
+    spin_sleep_ms(1000);
+  }
 }
