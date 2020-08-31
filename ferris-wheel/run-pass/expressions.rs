db7c3b6415143222ddc8655054552ad8aef74a83
akashin <kashin.andrej@gmail.com>
2020-08-31 16:35:19 +0100

    [ferris-wheel] Fix run-pass/expressions.rs

--- ferris-wheel/run-pass/expressions.rs
+++ ferris-wheel/run-pass/expressions.rs
@@ -5,9 +5,17 @@
 #[derive(Debug, PartialOrd, PartialEq, Clone, Copy)]
 struct IntWrapper(isize);
 
+impl Eq for IntWrapper { }
+
+impl Ord for IntWrapper {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(&other).expect("comparison of integers must be total")
+    }
+}
+
 pub fn main() {
-    assert_eq!(max(1usize, 3), 3);
-    assert_eq!(max(1u8, 3), 3);
-    assert_eq!(max(1u8, 3), 3);
-    assert_eq!(max(IntWrapper(120), IntWrapper(248)), IntWrapper(248));
+    assert_eq!(std::cmp::max(1usize, 3), 3);
+    assert_eq!(std::cmp::max(1u8, 3), 3);
+    assert_eq!(std::cmp::max(1u8, 3), 3);
+    assert_eq!(std::cmp::max(IntWrapper(120), IntWrapper(248)), IntWrapper(248));
 }
